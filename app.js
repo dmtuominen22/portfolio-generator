1687afa6d8834d4462765888b823ba974f62be49
Denise <dmtuominen@gmail.com>
2021-08-07 12:15:18 -0600

    added input 9.1

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello Node!");
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    // This...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+  
+    // Is the same as this...
+    profileDataArr.forEach((profileItem) => {
+      console.log(profileItem)
+    });
+  };
