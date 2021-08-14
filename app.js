b153347bdee23edac040f8bfae54f9dd270925a6
Denise <dmtuominen@gmail.com>
2021-08-14 16:48:14 -0600

    fixed extra folders

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+app
+
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 const { writeFile, copyFile } = require('./utils/generate-site');
@@ -144,22 +146,3 @@ promptUser()
   .catch(err => {
     console.log(err);
   });
-promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    return generatePage(portfolioData);
-  })
-  .then(pageHTML => {
-    return writeFile(pageHTML);
-  })
-  .then(writeFileResponse => {
-    console.log(writeFileResponse);
-    return copyFile();
-  })
-  .then(copyFileResponse => {
-    console.log(copyFileResponse);
-  })
-  .catch(err => {
-    console.log(err);
-  });
-    
